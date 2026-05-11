118221802dd37782673c3bc68a91ce2c7909a435
Nguyen Hai Son <sonnguyennr111@gmail.com>
2026-05-12 00:24:28 +0700

    Update

--- src/main/resources/static/nhom7js/nhom7auth.js
+++ src/main/resources/static/nhom7js/nhom7auth.js
@@ -1,25 +1,42 @@
 const userArea = document.getElementById("nhom7-user-area");
 
-const username = localStorage.getItem("nhom7_username");
+const username =
+    localStorage.getItem("nhom7_username");
 
 if(username){
 
     userArea.innerHTML = `
-        <span>
-            Xin chào, ${username}
-        </span>
 
-        <button onclick="nhom7Logout()">
-            Đăng xuất
-        </button>
+        <div class="nhom7-user-box">
+
+            <div class="nhom7-user-icon">
+
+            </div>
+
+            <div class="nhom7-user-info">
+
+                <span>
+                    Tài khoản
+                </span>
+
+                <button onclick="nhom7Logout()">
+                    Đăng xuất
+                </button>
+
+            </div>
+
+        </div>
+
     `;
 
 }else{
 
     userArea.innerHTML = `
+
         <a href="/nhom7login.html">
             Đăng nhập
         </a>
+
     `;
 }
 
